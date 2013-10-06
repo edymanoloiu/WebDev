77e66638bb109ac9bb1fd157cc2a8e70296626fc
Edi Manoloiu <edymanoloiu@gmail.com>
2013-10-06 12:49:37 +0300

    changed implementation

--- WebDev/src/Main.java
+++ WebDev/src/Main.java
@@ -2,44 +2,64 @@ import java.util.LinkedList;
 import java.util.StringTokenizer;
 
 public class Main {
+	
+	
+	public double add_two(int x) {
+		return x + 2;
+	}
+	
+	public double sqrt(int x) {
+		return Math.sqrt(x);
+	}
+	public double divide_by_five(int x) {
+		return x / 5;
+	}
+	public double multiply_by_four(int x) {
+		return x * 4;
+	}
+	public double substract_ten(int x) {
+		return x - 10;
+	}
+	public double squered(int x) {
+		return Math.pow(x, 2);
+	}
 
 	public double parseFunction(String function, int val) {
 		if (function.equals("sqrt"))
-			return Math.sqrt(val);
-		else {
-			StringTokenizer stringTokenizer = new StringTokenizer(function,
-					"+/*-^");
-			int x = Integer.parseInt(stringTokenizer.nextToken());
-			if(function.contains("/"))		
-				return val / x;
-			else
-				if(function.contains("*"))
-					return val * x;
-				else
-					if(function.contains("^"))
-						return Math.pow(val, x);
-					else 
-						if(function.contains("+"))
-							return val + x;
-						else 
-							return val - x;
-		}
-		
-
+			return sqrt(val);
+		if(function.equals("add_two"))
+			return add_two(val);
+		if(function.equals("squered"))
+			return squered(val);
+		if(function.equals("substract_ten"))
+			return substract_ten(val);
+		if(function.equals("multiply_by_four"))
+			return multiply_by_four(val);
+		if(function.equals("divide_by_five"))
+			return divide_by_five(val);
+		return Integer.MIN_VALUE;
 	}
 
 	public static void main(String[] args) {
+		Main process = new Main();
 		int x = 25;
 		LinkedList<String> functions = new LinkedList<>();
-		functions.add("+2");
-		functions.add("*10");
-		functions.add("-4");
+		LinkedList<Double> results = new LinkedList<>();
+		functions.add("add_two");
+		functions.add("multiply_by_four");
 		functions.add("sqrt");
-		functions.add("/5");
-		functions.add("^2");
 		for (String function : functions) {
-			System.out.println(new Main().parseFunction(function, x));
+			results.add(process.parseFunction(function, x));
 		}
+		System.out.println(results);
+		functions.add("substract_ten");
+		functions.add("divide_by_five");
+		functions.add("squered");
+		functions.add("no_existing_function");
+		for (String function : functions) {
+			results.add(process.parseFunction(function, x));
+		}
+		System.out.println(results);
 
 	}
 }
